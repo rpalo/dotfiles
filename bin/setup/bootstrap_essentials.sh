b1ceb4077e5cf91bd190d3218d5cb4ac4f1947dc
Ryan Palo <ryan@thepalos.com>
2020-01-24 10:42:09 -0800

    Update bootstraps with formatting and cronjob stuff

--- bin/setup/bootstrap_essentials.sh
+++ bin/setup/bootstrap_essentials.sh
@@ -4,7 +4,11 @@
 # 
 # Get everything set up so that yadm can take over
 
-echo "=== Updating ==="
+echo "### Bootstrap Essentials"
+echo "### Setting up your computer to be bootstrapped..."
+
+echo "## Updating packages..."
+# TODO: Silence and tidy output
 sudo apt update && sudo apt upgrade
 
 required_packages=(
@@ -15,11 +19,11 @@ required_packages=(
   yadm
 )
 
-echo "=== Installing the requested packages ==="
+echo "## Installing the requested packages... "
 sudo apt install "${required_packages[@]}" 
 
-echo "=== Essential bootstrap complete! ==="
+echo "### Essential bootstrap complete!"
 
-echo "=== Cloning dotfiles ==="
+echo "### Cloning dotfiles and running main bootstrap..."
 yadm clone https://github.com/rpalo/dotfiles.git --bootstrap
 
